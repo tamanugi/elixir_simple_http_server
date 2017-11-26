e8eab9717a4946bb3a22bee8451a8d8bd3e84cb7
tamanugi <tamanugi@gmail.com>
2017-11-26 16:08:07 +0900

    implement tcp echo server

--- lib/elixir_simple_http_server.ex
+++ lib/elixir_simple_http_server.ex
@@ -1,18 +1,31 @@
 defmodule ElixirSimpleHttpServer do
-  @moduledoc """
-  Documentation for ElixirSimpleHttpServer.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirSimpleHttpServer.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def start(port \\ 80) do
+    {:ok, lsocket} = :gen_tcp.listen(port,  [:binary, packet: :line, active: false, reuseaddr: true])
+    loop_acceptor(lsocket)
   end
+
+  def loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
+
+  def serve(socket) do
+    socket
+    |> read_line
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  def read_line(socket) do
+    {:ok, message} = :gen_tcp.recv(socket, 0)
+    message
+  end
+
+  def write_line(message, socket) do
+    :gen_tcp.send(socket, message)
+  end
+
 end
